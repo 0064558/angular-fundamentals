b11981e5df8d6393b39443c10ede0a6348632020
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-08-24 15:42:35 -0300

    feat: refactor user selection to use signals for reactive updates

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import {DUMMY_USERS} from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -10,16 +10,22 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  // propriedade que vai armazenar o usuário selecionado aleatoriamente
-  selectedUser = DUMMY_USERS[randomIndex];
+  // propriedade que vai armazenar o usuário selecionado por meio de um signal, 
+  // que é uma função que retorna um valor que pode ser atualizado e observado
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // propriedade que vai armazenar o caminho da imagem do usuário selecionado por meio de um signal computado, 
+  // que é uma função que retorna um valor derivado de outros sinais
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
 
   // propriedade que vai armazenar o caminho da imagem do usuário selecionado
-  get imagePath() {
+  /*get imagePath() {
     return 'assets/users/' + this.selectedUser.avatar;
-  }
+  }*/
 
+    // método que vai selecionar um usuário aleatoriamente e atualizar o sinal selectedUser
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // this.selectedUser = DUMMY_USERS[randomIndex];
   }
 }
