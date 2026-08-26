b77741296a5984271a9ec6a7f6d12465fbf05831
Rodrigo Alexandre <rodrigo11.vgp@gmail.com>
2026-08-26 16:25:17 -0300

    feat: implement constructor to load tasks from local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -30,6 +30,15 @@ export class TasksService {
         },
     ];
 
+    constructor() {
+        const tasks = localStorage.getItem('tasks');
+
+        if (tasks) {
+            this.tasks = JSON.parse(tasks);
+        }
+
+    }
+
     getUserTasks(userId: string) {
         return this.tasks.filter((task) => task.userId === userId);
     }
@@ -41,12 +50,17 @@ export class TasksService {
             title: taskData.title,
             summary: taskData.summary,
             dueDate: taskData.dueDate,
-        })
+        });
+        this.saveTasksToLocalStorage();
     }
 
     removeTask(taskId: string) {
         this.tasks = this.tasks.filter((task) => task.id !== taskId);
+        this.saveTasksToLocalStorage();
     }
 
-    
+
+    private saveTasksToLocalStorage() {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
 }
